fd33566df6012f3c1706f81172b304d5a7e9b3a4
host1812 <host1812@gmail.com>
2022-01-06 05:31:45 +0000

    wip: tickscript works

--- scripts/influx_data_generator/influx_data_generator.py
+++ scripts/influx_data_generator/influx_data_generator.py
@@ -1,10 +1,20 @@
+import os
+import re
 # import requests
 
 
 DEFAULT_FILE_NAME = "data.influx"
 
-with DEFAULT_FILE_NAME as f:
-    lines = open(f).readlines
+influx_data_file = os.path.join(os.path.dirname(os.path.realpath(__file__)),DEFAULT_FILE_NAME)
+
+with open(influx_data_file) as f:
+    lines = f.readlines()
 
 for line in lines:
     print(f"line: {line}")
+    if "$random" in line:
+        print("random token detected")
+        random_funcs = re.findall(r"\$random\(.*\)", line)
+        for rand_func in random_funcs:
+            min, max = re.findall(r"\d+", rand_func)
+            print(f"min: {min}, max: {max}")
