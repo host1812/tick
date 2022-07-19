7e8a73a8f0b23dd0caac76f70fef374b330fbc6a
host1812 <host1812@gmail.com>
2022-07-19 15:31:01 -0700

    basic functionality to list all databases added

--- scripts/check_retention_policies/check_retention_policies.sh
+++ scripts/check_retention_policies/check_retention_policies.sh
@@ -1,3 +1,20 @@
+#!/bin/bash
+
 # gets list of all databases and
 # checks retention policies for each
+# needs to be run as root
 
+function log {
+    echo "$(date --iso-8601=seconds) ${*}"
+}
+
+databases=($(docker exec -it influxdb influx \
+    -ssl \
+    -unsafeSsl \
+    -precision=rfc3339 \
+    -execute "SHOW DATABASES"))
+
+for db in ${databases[@]:4}
+do
+    log "db name: ${db}"
+done
